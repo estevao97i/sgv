243650ad5a46efe2e31dbba98cc3cc89b8f109a5
Estevao Araujo <estevao.araujo@basis.com.br>
2022-03-30 22:25:13 -0300

    upload project

---
+++ back-end/src/main/java/com/sgv/sistema_gerenciador_vendas/SistemaGerenciadorVendasApplication.java
@@ -0,0 +1,13 @@
+package com.sgv.sistema_gerenciador_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciadorVendasApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/sgv/sistema_gerenciador_vendas/SistemaGerenciadorVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgv.sistema_gerenciador_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
